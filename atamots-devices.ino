5c960a86b83121476a49e554e45c23d041a35957
rochmadnf <rochmadnf@gmail.com>
2022-06-13 21:13:48 +0800

    tes menggunakan multi ultraasonic

--- atamots-devices.ino
+++ atamots-devices.ino
@@ -1,9 +1,44 @@
+const int echoPins[3] = { 34, 35, 36 };
+const int trigPins[3] = { 18, 19, 5 };
+
+float distances[3];
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  for(byte pinNumber = 0; pinNumber < 3; pinNumber++){
+    pinMode(echoPins[pinNumber], INPUT);
+    pinMode(trigPins[pinNumber], OUTPUT);
+  }  
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  for(byte i = 0; i < 3; i++){
+    distances[i] = getDistance(trigPins[i], echoPins[i]);
+    delay(500);
+  }
 
+  Serial.println(String(distances[0]) + " | " + String(distances[1]) + " | " + String(distances[2]) + " | ");
+  delay(10000);
+  
+
+}
+
+float getDistance(byte trig, byte echo){
+  //  local variable
+  long duration;
+  float distance;
+  
+  digitalWrite(trig, LOW);
+  delayMicroseconds(20);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(100);
+  digitalWrite(trig, LOW);
+
+  duration = pulseIn(echo, HIGH);
+
+  distance = duration * (0.034/2);
+  Serial.println("Pin Trig : " + String(trig) + ", D : " + String(distance));
+  return distance;
 }
